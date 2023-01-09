1601565f4f4d1316018257ea4b052ea1be4c6307
PetrovitchSharp <korchagin_official@yahoo.com>
2023-01-09 15:39:01 +0400

    added new fields to mongo records

--- lido_parser/main.py
+++ lido_parser/main.py
@@ -1,10 +1,11 @@
 from typing import List, Dict, Tuple
+from datetime import datetime
 
 from pymongo import MongoClient, Database
 from web3 import Web3
 from lido_sdk import Lido
 
-from cfg import CONNECTION_STRING, TABLE_NAME, INFURA_PROJECT_ID
+from cfg import CONNECTION_STRING, TABLE_NAME, NODE_CONNECTION_STRING
 
 
 def get_mongo_table(connection_string: str, table_name: str) -> Tuple[MongoClient, Database]:
@@ -22,20 +23,20 @@ def get_mongo_table(connection_string: str, table_name: str) -> Tuple[MongoClien
     return client, client[f'{table_name}']
 
 
-def get_lido_validators(infura_project_id: str) -> List[str]:
+def get_lido_validators(node_connection_string: str) -> List[str]:
     '''
     Get list of LIDO validators wallets
 
     Args:
-        infura_project_id: API key for Infura
+        node_connection_string: Connection string of ETH node
 
     Returns:
         List of LIDO validators' ETH wallets addresses   
     '''
 
-    # Connect to Infura API
+    # Connect to ETH Node
     w3 = Web3(Web3.HTTPProvider(
-        f'https://mainnet.infura.io/v3/{infura_project_id}'))
+        f'{node_connection_string}'))
     lido = Lido(w3)
 
     # Get list of indexes of LIDO validators
@@ -68,7 +69,11 @@ def list_to_json(validators_wallets: List[str]) -> List[Dict]:
         Returns:
             Wrapped address
         '''
-        return {'eth_wallet_address': wallet}
+        return {
+            'eth_wallet_address': wallet,
+            'addition_date': datetime.now(),
+            'delition_date': None
+        }
 
     return list(map(wrapper, validators_wallets))
 
@@ -84,25 +89,49 @@ def get_difference(table: Database, validators_wallets: List[str]) -> Tuple[List
     Returns:
         Lists of wallets addressed to drop from table and to add to it
     '''
-    # Get all records from table
-    cursor = table.find({})
-    wallets_in_table = cursor.distinct('eth_wallet_address')
+    # Get all active records from table
+    cursor = table.find({}).where('this.delition_date == null')
+    active_wallets_in_table = cursor.distinct('eth_wallet_address')
 
     # Find difference between lists
-    wallets_to_drop = list(set(validators_wallets) - set(wallets_in_table))
-    wallets_to_add = list(set(wallets_in_table) - set(validators_wallets))
+    wallets_to_drop = list(set(active_wallets_in_table) - set(validators_wallets))
+    wallets_to_add = list(set(validators_wallets) - set(active_wallets_in_table))
 
     return wallets_to_drop, wallets_to_add
 
 
+def make_inactive(table: Database, wallets_to_drop: List[str]) -> int:
+    '''
+    Make wallet inactive
+
+    Args:
+        table:              Instance of db table
+        wallets_to_drop:    List of LIDO validators' ETH wallets addresses to make inactive
+
+    Returns:
+        Count of wallets were made inactive
+    '''
+    cursor = table.find({})
+    deleted_count = 0
+    
+    for wallet in wallets_to_drop:
+        wallet_id = cursor.where(f'this.delition_date == null && this.eth_wallet_address == {wallet}').distinct('_id')
+        table.update_one({'_id' : wallet_id}, {'$set': {'delition_date': datetime.now()}})   
+        
+        deleted_count += 1     
+        
+    return(deleted_count)
+
+
+
 if __name__ == '__main__':
     connection, table = get_mongo_table(CONNECTION_STRING, TABLE_NAME)
-    validators_wallets = get_lido_validators(INFURA_PROJECT_ID)
+    validators_wallets = get_lido_validators(NODE_CONNECTION_STRING)
     wallets_to_drop, wallets_to_add = get_difference(table, validators_wallets)
-    drop_res = table.delete_many(list_to_json(wallets_to_drop))
+    drop_res = make_inactive(wallets_to_drop)
     add_res = table.insert_many(list_to_json(wallets_to_add))
 
-    print(f'Deleted {drop_res.deleted_count} wallets to db')
-    print(f'Added {len(add_res.inserted_ids)} wallets to db')
+    print(f'{drop_res} wallets were made inactive')
+    print(f'Added {len(add_res.inserted_ids)} active wallets to db')
 
     connection.close()
