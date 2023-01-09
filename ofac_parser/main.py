ed1ed943d05b1e3c542bdca0be7c7437b47fc940
PetrovitchSharp <korchagin_official@yahoo.com>
2023-01-09 15:57:48 +0400

    totally changed format of db records

--- ofac_parser/main.py
+++ ofac_parser/main.py
@@ -8,7 +8,10 @@ from pymongo import MongoClient, Database
 from cfg import CONNECTION_STRING, TABLE_NAME, OFAC_LIST_URL
 
 
-def get_mongo_table(connection_string: str, table_name: str) -> Tuple[MongoClient, Database]:
+def get_mongo_table(
+    connection_string: str,
+    table_name: str
+) -> Tuple[MongoClient, Database]:
     '''
     Get MongoDB connection and table
 
@@ -17,7 +20,7 @@ def get_mongo_table(connection_string: str, table_name: str) -> Tuple[MongoClien
         table_name:         Name of table inside MongoDB
 
     Returns:
-        Connection object and the instance of required table     
+        Connection object and the instance of required table
     '''
     client = MongoClient(connection_string)
     return client, client[f'{table_name}']
@@ -28,10 +31,10 @@ def get_banned_wallets(ofac_list_url: str) -> List[str]:
     Get list of ETH wallets banned by OFAC
 
     Args:
-        ofac_list_url: URL of complete OFAC SDN list 
+        ofac_list_url: URL of complete OFAC SDN list
 
     Returns:
-        List of ETH wallets addresses   
+        List of ETH wallets addresses
     '''
     def truncate_address(address: str) -> str:
         '''
@@ -61,88 +64,26 @@ def get_banned_wallets(ofac_list_url: str) -> List[str]:
 
 def list_to_json(banned_wallets: List[str]) -> List[Dict]:
     '''
-    Transform list of addresses to list of json objects
+    Transform list of addresses to json object
 
     Args:
         banned_wallets: List of banned ETH wallets addresses
 
     Returns:
-        List of banned ETH wallets addresses in json format
+        JSON with list of banned wallets
     '''
-    def wrapper(wallet: str) -> Dict:
-        '''
-        Wrap wallet address in json
 
-        Args:
-            wallet: ETH wallet address
+    return {
+        'dt': datetime.utcnow,
+        'banned_wallets': banned_wallets
+    }
 
-        Returns:
-            Wrapped address
-        '''
-        return {
-            'eth_wallet_address': wallet,
-            'addition_date': datetime.now(),
-            'delition_date': None
-        }
-
-    return list(map(wrapper, banned_wallets))
-
-
-def get_difference(table: Database, banned_wallets: List[str]) -> Tuple[List[str], List[str]]:
-    '''
-    Get difference between addressed in db table and current banlist
-
-    Args:
-        table:          Instance of db table
-        banned_wallets: List of banned wallets addressed
-
-    Returns:
-        Lists of wallets addressed to drop from table and to add to it
-    '''
-    # Get all active records from table
-    cursor = table.find({}).where('this.delition_date == null')
-    active_wallets_in_table = cursor.distinct('eth_wallet_address')
-
-    # Find difference between lists
-    wallets_to_drop = list(
-        set(active_wallets_in_table) - set(banned_wallets))
-    wallets_to_add = list(set(banned_wallets) -
-                          set(active_wallets_in_table))
-
-    return wallets_to_drop, wallets_to_add
-
-def make_inactive(table: Database, wallets_to_drop: List[str]) -> int:
-    '''
-    Make wallet inactive
-
-    Args:
-        table:              Instance of db table
-        wallets_to_drop:    List of banned validators' ETH wallets addresses to make inactive
-
-    Returns:
-        Count of wallets were made inactive
-    '''
-    cursor = table.find({})
-    deleted_count = 0
-
-    for wallet in wallets_to_drop:
-        wallet_id = cursor.where(
-            f'this.delition_date == null && this.eth_wallet_address == {wallet}').distinct('_id')
-        table.update_one({'_id': wallet_id}, {
-                         '$set': {'delition_date': datetime.now()}})
-
-        deleted_count += 1
-
-    return(deleted_count)
 
 if __name__ == '__main__':
     connection, table = get_mongo_table(CONNECTION_STRING, TABLE_NAME)
     banned_wallets = get_banned_wallets(OFAC_LIST_URL)
-    wallets_to_drop, wallets_to_add = get_difference(table, banned_wallets)
-    drop_res = make_inactive(wallets_to_drop)
-    add_res = table.insert_many(list_to_json(wallets_to_add))
+    add_res = table.insert_one(list_to_json(banned_wallets))
 
-    print(f'{drop_res} wallets were made inactive')
-    print(f'Added {len(add_res.inserted_ids)} active wallets to db')
+    print(f'{datetime.utcnow}: Added list of {len(banned_wallets)} banned wallets to db')
 
     connection.close()
