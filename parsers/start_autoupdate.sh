1412d97345e9b317c16f90441002fc57be0af58b
PetrovitchSharp <korchagin_official@yahoo.com>
2023-01-17 12:03:45 +0400

    wip: docker

--- parsers/start_autoupdate.sh
+++ parsers/start_autoupdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -n 86400 poetry run python lido_parser.py & watch -n 86400 poetry run python ofac_parser.py
+watch -n 86400 poetry run python3 lido_parser.py & watch -n 86400 poetry run python3 ofac_parser.py
