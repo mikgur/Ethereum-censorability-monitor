f3be710870f10d91ec75225c1ddab4a55b85e9aa
Mikhail Gurevich <gurevich3004@gmail.com>
2023-02-13 18:38:23 +0300

    Add collector classes

--- censorability_monitor/data_collection/collector.py
+++ censorability_monitor/data_collection/collector.py
@@ -6,7 +6,9 @@ from multiprocessing import current_process
 from typing import List
 
 from pymongo import MongoClient
+from pymongo import UpdateOne
 from web3.auto import Web3
+from web3.exceptions import TransactionNotFound
 
 logger = logging.getLogger(__name__)
 
@@ -14,15 +16,38 @@ logger = logging.getLogger(__name__)
 class DataCollector:
     '''Base class for data collectors'''
     def __init__(self, mongo_url: str, web3_type: str, web3_url: str,
-                 interval: float, name: str = 'DataCollector'):
+                 interval: float, verbose: bool, name: str = 'DataCollector'):
         self.mongo_url = mongo_url
         self.web3_type = web3_type
         self.web3_url = web3_url
         self.interval = interval
+        self.verbose = verbose
         self.name = name
 
     def run(self):
         asyncio.run(self.collect())
+
+    async def wait_if_needed(self, t1: float):
+        logger = logging.getLogger(self.name)
+        t2 = time.time()
+        time_left = self.interval - (t2 - t1)
+        if time_left < 0:
+            logger.warning(f'Slow collector: {self.name}')
+        await asyncio.sleep(max(time_left, 0))
+
+    def get_mongo_client(self):
+        return MongoClient(self.mongo_url)
+
+    def get_web3_client(self):
+        logger = logging.getLogger(self.name)
+        if self.web3_type == 'ipc':
+            w3 = Web3(Web3.IPCProvider(self.web3_url))
+            logger.info(f'Connected to ETH node: {w3.isConnected()}')
+            return w3
+        else:
+            msg = f'Unexpected web3 connection type: {self.web3_type}'
+            logger.error(msg)
+            raise Exception(msg)
 
     async def collect(self):
         raise NotImplementedError
@@ -32,26 +57,21 @@ class MempoolCollector(DataCollector):
     '''Collects transactions from the mempool
        and stores the first seen timestamp in MongoDB'''
     def __init__(self, mongo_url: str, web3_type: str, web3_url: str,
-                 interval: float = 0.5):
+                 interval: float = 0.5, verbose: bool = True):
         super().__init__(mongo_url, web3_type, web3_url,
-                         interval, 'MempoolCollector')
+                         interval, verbose, 'MempoolCollector')
 
     async def collect(self):
         # Connect to the ETH node and MongoDB
-        logger = logging.getLogger('MempoolCollector')
-        if self.web3_type == 'ipc':
-            self.w3 = Web3(Web3.IPCProvider(self.web3_url))
-            self.mongo_client = MongoClient(self.mongo_url)
-        else:
-            msg = f'Unexpected web3 connection type: {self.web3_type}'
-            logger.error(msg)
-            raise Exception(msg)
-        logger.info(f'Connected to ETH node: {self.w3.isConnected()}')
+        logger = logging.getLogger(self.name)
+        mongo_client = self.get_mongo_client()
+        w3 = self.get_web3_client()
+        logger.info('Start collecting mempool data')
 
         # Get the collections
-        db = self.mongo_client['ethereum_mempool']
+        db = mongo_client['ethereum_mempool']
         first_seen_collection = db['tx_first_seen_ts']
-        tx_filter = self.w3.eth.filter('pending')
+        tx_filter = w3.eth.filter('pending')
         while True:
             t1 = time.time()
             new_transactions = tx_filter.get_new_entries()
@@ -63,46 +83,224 @@ class MempoolCollector(DataCollector):
             new_hashes = [h for h in new_transactions
                           if h not in existing_hashes]
             # Prepare data for insertion
-            new_transactions_first_seen = [{'hash': h, 'timestamp': t1}
+            new_transactions_first_seen = [{'hash': h, 'timestamp': int(t1)}
                                            for h in new_hashes]
+            # Add details to new transactions
+            details_not_found = 0
+            for tx in new_transactions_first_seen:
+                try:
+                    tx_data = w3.eth.getTransaction(tx['hash'])
+                    tx['from'] = tx_data['from']
+                    tx['nonce'] = tx_data['nonce']
+                    if 'maxFeePerGas' in tx_data:
+                        tx['maxFeePerGas'] = tx_data['maxFeePerGas']
+                    else:
+                        tx['maxFeePerGas'] = tx_data['gasPrice']
+                except TransactionNotFound:
+                    details_not_found += 1
+                    continue
+            n_new_txs = len(new_transactions_first_seen)
+            details_found = n_new_txs - details_not_found
+
             # Insert new transactions
             if new_transactions_first_seen:
                 first_seen_collection.insert_many(new_transactions_first_seen)
             n_inserted = len(new_transactions_first_seen)
-            logger.info((f'Inserted {n_inserted} new '
-                         f'transactions from {n} total'))
+            if self.verbose:
+                logger.info((f'Inserted {n_inserted} new txs, '
+                             f'found {details_found}/{n_new_txs}'
+                             f'txs from {n} total'))
             t2 = time.time()
             time_left = self.interval - (t2 - t1)
             if time_left < 0:
                 logger.warning(f'Slow collector: {current_process().name}')
             await asyncio.sleep(max(time_left, 0))
+
+
+class AddressDataCollector:
+    def __init__(self, web3_type: str, web3_url: str):
+        self.logger = logging.getLogger('AddressDataCollector')
+        self.web3_type = web3_type
+        self.web3_url = web3_url
+
+    def get_web3_client(self):
+        if self.web3_type == 'ipc':
+            w3 = Web3(Web3.IPCProvider(self.web3_url))
+            return w3
+        else:
+            msg = f'Unexpected web3 connection type: {self.web3_type}'
+            self.logger.error(msg)
+            raise Exception(msg)
+
+    def get_address_data(self, addresses: List[str], block_number: int):
+        w3 = self.get_web3_client()
+        result = {}
+        for address in addresses:
+            n_transactions = w3.eth.get_transaction_count(
+                address, block_number)
+            eth_account = w3.eth.get_balance(
+                address, block_number) / 10 ** 18
+            result[address] = {'n_txs': n_transactions,
+                               'eth': eth_account}
+        return result
+
+
+def split_on_chunks(a: List, chunk_size: int):
+    # looping till length l
+    for i in range(0, len(a), chunk_size):
+        yield a[i:i + chunk_size]
 
 
 class BlockCollector(DataCollector):
     def __init__(self, mongo_url: str, web3_type: str, web3_url: str,
-                 interval: float = 3):
+                 interval: float = 3, verbose: bool = True):
         super().__init__(mongo_url, web3_type, web3_url,
-                         interval, 'BlockCollector')
+                         interval, verbose, 'BlockCollector')
+        self.max_workers = 256
+        self.address_data_collectors = [
+            AddressDataCollector(web3_type, web3_url)
+            for _ in range(self.max_workers)
+        ]
 
     async def collect(self):
-        logger = logging.getLogger('BlockCollector')
-        if self.web3_type == 'ipc':
-            self.w3 = Web3(Web3.IPCProvider(self.web3_url))
-            self.mongo_client = MongoClient(self.mongo_url)
-        else:
-            msg = f'Unexpected web3 connection type: {self.web3_type}'
-            logger.error(msg)
-            raise Exception(msg)
+        logger = logging.getLogger(self.name)
+        mongo_client = self.get_mongo_client()
+        w3 = self.get_web3_client()
 
-        logger.info(f'Connected to ETH node: {self.w3.isConnected()}')
+        last_processed_block = w3.eth.blockNumber - 1
         while True:
             t1 = time.time()
-            logger.info('BlockCollector ping')
+            current_block = w3.eth.blockNumber
+            if current_block > last_processed_block:
+                for block_number in range(last_processed_block + 1,
+                                          current_block + 1):
+                    await self.process_block_data(
+                        block_number, w3, mongo_client)
+                last_processed_block = current_block
             t2 = time.time()
             time_left = self.interval - (t2 - t1)
             if time_left < 0:
                 logger.warning(f'Slow collector: {current_process().name}')
             await asyncio.sleep(max(time_left, 0))
+
+    async def process_block_data(self, block_number: int,
+                                 w3: Web3, mongo_client: MongoClient):
+        logger = logging.getLogger(self.name)
+        logger.info(f'Processing block {block_number}')
+        db = mongo_client['ethereum_mempool']
+        first_seen_collection = db['tx_first_seen_ts']
+        block = w3.eth.getBlock(block_number)
+        block_ts = block['timestamp']
+        # Get transactions from mempool that are not in the blocks
+        # and update their from accounts data
+        transactions = first_seen_collection.find(
+            {'timestamp': {'$lte': block_ts},
+             'block_number': {'$exists': False}})
+        # Get mempool accounts and remove old txs without details
+        no_details = 0
+        n_mempool_txs = 0
+        remove_from_mempool = []
+        mempool_accounts = set()
+        low_fee_txs = 0
+        old_txs_found = 0
+        # Remove old txs without details
+        # Try to find deails for txs without details
+        # Get list of addresses with txs with enough gas price
+        for tx in transactions:
+            n_mempool_txs += 1
+            if 'from' not in tx:
+                try:
+                    tx_data = w3.eth.getTransaction(tx['hash'])
+                    found_tx = {}
+                    found_tx['from'] = tx_data['from']
+                    found_tx['nonce'] = tx_data['nonce']
+                    if 'maxFeePerGas' in tx_data:
+                        found_tx['maxFeePerGas'] = tx_data['maxFeePerGas']
+                    else:
+                        found_tx['maxFeePerGas'] = tx_data['gasPrice']
+                    first_seen_collection.update_one(
+                        {'hash': tx['hash']},
+                        {'$set': found_tx})
+                    old_txs_found += 1
+                except TransactionNotFound:
+                    no_details += 1
+                # Remove tx that doesn't have details after 60 seconds
+                if block_ts - tx['timestamp'] > 60:
+                    remove_from_mempool.append(tx['hash'])
+                continue
+            # check that tx maxGasPrice is higher than blocks BaseFeePerGas
+            if ('maxFeePerGas' in tx
+                    and tx['maxFeePerGas'] < block['baseFeePerGas']):
+                low_fee_txs += 1
+                continue
+            mempool_accounts.add(tx['from'])
+        logger.info(f'Found {old_txs_found} old transactions')
+        n_removed = len(remove_from_mempool)
+        # Remove old enough txs without details
+        first_seen_collection.delete_many(
+            {'hash': {'$in': remove_from_mempool}})
+        logger.info((f'Found {no_details}/{n_mempool_txs} txs without '
+                     f'details, remove {n_removed} from mempool. '))
+        logger.info(f'Interesting accs in mempool: {len(mempool_accounts)}')
+
+        # Update accounts info:
+        t1 = time.time()
+        batch_size = 1000
+        num_workers = min(len(mempool_accounts) // batch_size + 1,
+                          self.max_workers)
+        process_executor = ProcessPoolExecutor(max_workers=num_workers)
+        event_loop = asyncio.get_event_loop()
+        chunks = list(split_on_chunks(list(mempool_accounts), batch_size))
+        address_data = {}
+        for i in range(0, len(chunks), num_workers):
+            current_chunks = chunks[i:i + num_workers]
+            collection_tasks = [
+                event_loop.run_in_executor(
+                    process_executor,
+                    collector.get_address_data,
+                    chunk,
+                    block_number - 1
+                )
+                for collector, chunk in zip(self.address_data_collectors,
+                                            current_chunks)
+            ]
+            data = await asyncio.gather(*collection_tasks)
+            for d in data:
+                address_data.update(d)
+        # Save accounts info to db
+        accounts_collection = db['addresses_info']
+        accounts_collection.create_index('address', unique=True)
+        accounts_in_db = accounts_collection.find(
+            {'address': {'$in': list(mempool_accounts)}})
+        existing_accounts = {a['address']: a for a in accounts_in_db}
+        new_accounts = [a for a in mempool_accounts
+                        if a not in existing_accounts]
+        insert_records = [{'address': a, str(block_number - 1): address_data[a]
+                           } for a in new_accounts]
+        if len(insert_records) > 0:
+            accounts_collection.insert_many(insert_records)
+
+        update_documents = [
+            UpdateOne(
+                {'address': {'$eq': a}},
+                {'$set':
+                    {f'{block_number - 1}.n_txs': address_data[a]['n_txs'],
+                     f'{block_number - 1}.eth': address_data[a]['eth']}
+                 }
+            )
+            for a in existing_accounts
+        ]
+        if len(update_documents) > 0:
+            accounts_collection.bulk_write(update_documents)
+
+        # Add block number to transactions included in the current block
+        result = first_seen_collection.update_many(
+            {'hash': {'$in': block['transactions']}},
+            {'$set': {'block_number': block_number}})
+        logger.info((f'Updated {result.modified_count} transactions of '
+                     f'{len(block["transactions"])} in block'))
+        logger.info((f'Processing with {num_workers} workers addresses '
+                     f'took {int(time.time() - t1)} s'))
 
 
 class CollectorManager:
