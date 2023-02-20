84a1cbae3d63bd990cb916b87fa2266a9fd4bbd2
PetrovitchSharp <korchagin_official@yahoo.com>
2023-02-20 12:34:28 +0400

    removed old web

--- web/censorred/censorred/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from validator_bars import views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("valbars/", views.index, name='bars')
-]
--- web/censorred/validator_bars/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ValidatorBarsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "validator_bars"
